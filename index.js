cfc18a706117eb3076e2e772463d6dadc1bf7795
Patrick Huijten <psbheh@gmail.com>
2021-04-09 20:06:27 +0200

    replace evaluate with page.type

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer')
 const dogFacts = require('dog-facts')
 const cron = require('node-cron')
 const fs = require('fs')
+const sleep = require('sleep-promise')
 const run = async () => {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -11,14 +12,13 @@ const run = async () => {
     await page.waitForSelector('#crisp-chatbox')
     await page.click('.cc-unoo')
     const randomFact = `Hourly random dog fact: ${dogFacts.random()}`
-    await page.evaluate((randomFact) => {
-        const field = document.querySelector('textarea[name=message]')
-        field.value = randomFact
-    }, randomFact)
+    page.type('textarea[name=message]', randomFact)
+
+    await sleep(2000)
 
     const date = new Date(Date.now())
     const time = date.getDate() + '_' + date.getHours()
-    
+
     await page.screenshot({ path: `${time}.png` })
 
     fs.writeFileSync(`${time}.log`, randomFact)
